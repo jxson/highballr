c8341bea332c5af843481545e5205801531b2188
Jason Campbell <jason@borderstylo.com>
2011-04-27 01:20:56 -0700

    ctrl+click is showing a nice menu and chatting to the global.html page

---
+++ highballr.safariextension/javascripts/injected.js
@@ -0,0 +1,9 @@
+console.debug('hi')
+
+// Gets info from the DOM
+function contextMenuHandler(event){
+  console.debug(event.target.nodeName)
+  safari.self.tab.setContextMenuEventUserInfo(event, event.target.nodeName);
+}
+
+document.addEventListener('contextmenu', contextMenuHandler, false);
