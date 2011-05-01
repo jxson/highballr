0ac8fa574e12b49f6c57aef6244bd46f11e0d013
Jason Campbell <jason@borderstylo.com>
2011-05-01 16:16:10 -0700

    experimental iframe injection since opening a window from inside a safari extension is busted

--- highballr.safariextension/javascripts/injected.js
+++ highballr.safariextension/javascripts/injected.js
@@ -5,3 +5,21 @@ function contextMenuHandler(event){
 }
 
 document.addEventListener('contextmenu', contextMenuHandler, false);
+
+function onMessage(event) {
+  if (event.name === 'injectHighballer') {
+    injectHighballer();
+  }
+}
+
+// http://developer.apple.com/library/safari/#documentation/Tools/Conceptual/SafariExtensionGuide/MessagesandProxies/MessagesandProxies.html%23//apple_ref/doc/uid/TP40009977-CH14-SW1
+safari.self.addEventListener('message', onMessage, false);
+
+var injectHighballer = function(params){
+  var frame = document.createElement('iframe');
+
+  frame.id = 'highballr-iframe';
+  frame.src = safari.extension.baseURI + 'highballr.html';
+
+  document.body.appendChild(frame);
+}
---
+++ notes.md
@@ -0,0 +1,2 @@
+* http://stackoverflow.com/questions/3399491/open-window-with-specified-width-height
+* http://stackoverflow.com/questions/4999075/safari-extension-open-url-in-a-new-window-active-tab
